0f1c3d893f9f9016ed4e649ad838f54b946321c1
Samuel Greloch <sgreloch@Zipcoders-MBP-4.hsd1.de.comcast.net>
2024-11-09 20:18:51 -0500

    done

---
+++ src/main/java/com/github/zipcodewilmington/sample/Wordguess.java
@@ -0,0 +1,31 @@
+package com.github.zipcodewilmington.sample;
+//Samuel Greloch
+
+import static java.lang.System.*;
+
+public class Wordguess {
+
+    public static void main(String[] args) {
+        Wordguess game = new Wordguess();
+        game.runGame();
+
+        String[] myName = {"rose", "joker", "simple", "supercalifragilisticexpialidocious" };
+        myName[0] = "rose";
+        myName[1] = "joker";
+        myName[2] = "simple";
+        myName[3] = "supercalifragilisticexpialidocious";
+
+
+        while ();
+
+    }
+
+
+
+    private void runGame() {
+    }
+    
+    static int getWordguess()
+    Scanner scan = new Scanner(Scanner.in);
+
+}
